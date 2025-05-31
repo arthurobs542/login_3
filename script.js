7a4beabc0d281a543e1c910d2af4c1908a844a37
Arthur Robson <arthurobson.contato@gmail.com>
2025-05-30 22:40:48 -0300

    colocando algumas funcionalidades no js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let b7valilador = {
   handleSubmit: (event) => {
-    event.preventDefalt();
+    event.preventDefault();
     let send = true;
 
     let inputs = form.querySelectorAll("input");
@@ -8,13 +8,28 @@ let b7valilador = {
     for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
       console.log(input);
+      let check = b7valilador.checkIpunt(input);
+      if (check !== true) {
+        send = false;
+        //exibir erro
+      }
     }
     send = false;
+
+    if (send) {
+      form.submit();
+    }
   },
-  if(send) {
-    form.submit();
+  checkIpunt: (input) => {
+    let rules = input.getAttribute("data-rules");
+    if (rules !== null) {
+    }
+    return true;
   },
 };
 
-let form = document.querySelector(".b7validador");
+console.log(b7valilador);
+
+let form = document.querySelector(".b7valilador");
+
 form.addEventListener("submit", b7valilador.handleSubmit);
