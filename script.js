5266ba789ac4d55157d6fe0dbe5f76e0e68fac8d
Arthur Robson <arthurobson.contato@gmail.com>
2025-05-31 12:31:55 -0300

    terminei js. finalizar com envio do formulário

--- script.js
+++ script.js
@@ -41,6 +41,17 @@ let valilador = {
               );
 
             break;
+
+          case "email":
+            if (input.value != "") {
+              let regex =
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+              if (!regex.test(input.value.toLowerCase())) {
+                return "E-mail digitado não é válido!";
+              }
+            }
+
+            break;
         }
       }
     }
