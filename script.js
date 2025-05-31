f3deb66dda90f096ebb237ce164548dd79ac7cff
Arthur Robson <arthurobson.contato@gmail.com>
2025-05-30 23:11:46 -0300

    atualizando js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let b7valilador = {
 
     for (let i = 0; i < inputs.length; i++) {
       let input = inputs[i];
-      console.log(input);
       let check = b7valilador.checkIpunt(input);
       if (check !== true) {
         send = false;
+        console.log(check);
         //exibir erro
       }
     }
-    send = false;
+    //send = false;
 
     if (send) {
       form.submit();
@@ -23,12 +23,24 @@ let b7valilador = {
   checkIpunt: (input) => {
     let rules = input.getAttribute("data-rules");
     if (rules !== null) {
+      rules = rules.split("|");
+      for (let k in rules) {
+        let rDetails = rules[k].split("=");
+        switch (rDetails[0]) {
+          case "required":
+            if (input.value == "") {
+              return "campo não pode ser vazio.";
+            }
+            break;
+
+          case "min":
+            break;
+        }
+      }
     }
     return true;
   },
 };
-
-console.log(b7valilador);
 
 let form = document.querySelector(".b7valilador");
 
